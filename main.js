c11aecec1c20af0c6db267d9739e598aae51f86d
Stephanie Snopek <creativesnopeks@gmail.com>
2017-09-18 16:36:52 -0700

    logic pt 2

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
 
-console.log("user flipped " + cardOne);
-console.log("user flipped " + cardFour);
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+
+console.log("user flipped " + cardTwo)
+console.log("user flipped " + cardOne)
+
+if(cardsInPlay.length === 2){
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("you found a match!")
+	} else alert("Sorry, try again")
+} 
