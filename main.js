81b58bd46b9372f025f2cb44c0d6e80ed7a86763
Stephanie Snopek <creativesnopeks@gmail.com>
2017-09-19 19:53:53 -0700

    logic pt 4

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		rank: "queen", 
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	}, 
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+
+	}, 
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+
+	}, 
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+
+	}
+];
 
 var cardsInPlay = [];
 
@@ -9,8 +33,10 @@ function checkForMatch(){
 }
 
 function flipCard(cardId){
-	console.log("User flipped " + cards[cardId])
-	cardsInPlay.push(cards[cardId])
+	console.log("User flipped " + cards[cardId].rank)
+	console.log(cards[cardId].cardImage)
+	console.log(cards[cardId].suit)
+	cardsInPlay.push(cards[cardId].rank)
 	if(cardsInPlay.length === 2){
 		checkForMatch()
 	}	
