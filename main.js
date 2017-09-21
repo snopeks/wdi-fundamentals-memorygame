e627cbe6a8729fca4c48ea615e35ba287e8506c1
Stephanie Snopek <creativesnopeks@gmail.com>
2017-09-21 12:04:55 -0700

    completed game logic

--- main.js
+++ main.js
@@ -28,22 +28,34 @@ var cardsInPlay = [];
 
 function checkForMatch(){
 	if(cardsInPlay[0] === cardsInPlay[1]){
-		console.log("you found a match!")	
-	} else console.log("Sorry, try again")
+		alert("you found a match!")	
+	} else alert("Sorry, try again")
 }
 
-function flipCard(cardId){
+function flipCard(){
+	var cardId = this.getAttribute('data-id')
 	console.log("User flipped " + cards[cardId].rank)
-	console.log(cards[cardId].cardImage)
-	console.log(cards[cardId].suit)
+	// console.log(cards[cardId].cardImage)
+	// console.log(cards[cardId].suit)
 	cardsInPlay.push(cards[cardId].rank)
+	this.setAttribute('src', cards[cardId].cardImage)
 	if(cardsInPlay.length === 2){
 		checkForMatch()
 	}	
 }
 
+function createBoard(){
+	for(var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i)
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
 
 
 
-flipCard(0)
-flipCard(2)
+
